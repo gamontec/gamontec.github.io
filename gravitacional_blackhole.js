5a64cb90d38668145479a6eeb0e59c3f88d41ffc
gamontec <gamontec@uc.cl>
2017-05-26 02:12:06 -0400

    crunch/bang

--- gravitacional_blackhole.js
+++ gravitacional_blackhole.js
@@ -3,7 +3,12 @@ var canvas;
 var particle=[];
 var x0;
 var y0;
-var numPart = 3000;
+var maxDiam = 4;
+var numPart = 4000;
+var release = false;
+var allIn = false;
+var vAux;
+var distTest;
 
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
@@ -14,30 +19,43 @@ function setup(){
   canvas.position(0,0);
   canvas.style('z-index','-1');
 
+  vAux = createVector();
+
   for (var i = 0; i < numPart; i++) {
     x0=random(-width/2, 1.5*width);
     y0=random(-width/2-height/2, 1.5*width);
 
-    while(dist(x0,y0,width/2,height/2)>width)
+    while(dist(x0,y0,width/2,height/2)>width) //tiene que ser width
     {
       x0=random(-width/2, 1.5*width);
       y0=random(-width/2, 1.5*width);
     }
 
-    particle[i] = new Particle(x0,y0,random(1,4));
+    particle[i] = new Particle(x0,y0,random(1,maxDiam));
   }
 }
 
 function draw(){
-background(30);
-for (var i = 0; i < numPart; i++) {
-  particle[i].blackHole();
-  particle[i].update();
-}
-//print(particle[0].v);
+  frameRate(60);
+
+  distTest=0; //for setting when every particle is at the middle
+  background(30);
+  for (var i = 0; i < numPart; i++) {
+    distTest += dist(particle[i].x,particle[i].y,width/2,height/2);
+    particle[i].update();
+  }
+  if (distTest < 1){
+    allIn=true;
+  }
+  //print(allIn);
+  //fill(30);
+  //noStroke();
+  //ellipse(width/2, height/2, maxDiam+1, maxDiam+1);
 }
 
+//Particle object
 function Particle(x,y,radius){
+  //attributes
   this.x=x;
   this.y=y;
   this.radius=radius;
@@ -46,8 +64,13 @@ function Particle(x,y,radius){
   this.ac=createVector(); //fuerza
   this.v=createVector();
   this.trapped = false;
+  this.inside = false;
 
+  //main method of every single particle
   this.update = function(){
+    if (release == false) {
+        this.blackHole();
+    }
 
     if (this.trapped == false) {
         this.move();
@@ -58,6 +81,7 @@ function Particle(x,y,radius){
     ellipse(this.x,this.y,this.radius,this.radius);
   }
 
+  //movement method. sets the movements phisics
   this.move = function(){
     //Set direction
     this.direction.x = width/2-this.x;
@@ -70,7 +94,7 @@ function Particle(x,y,radius){
       this.ac=this.dirNorm.mult(0);
     }
     else {
-      this.ac=this.dirNorm.mult(1/(2*distancia));
+      this.ac=this.dirNorm.mult(2/(distancia^2));
     }
 
     //Set velocity
@@ -82,14 +106,32 @@ function Particle(x,y,radius){
     this.y = this.y + this.v.y;
   }
 
+  //sets when every single particle is in the middle
   this.blackHole = function(){
     if (dist(this.x,this.y,width/2,height/2) <= 5) {
       this.trapped = true;
       this.ac.set(0,0);
       this.v.set(0,0);
-      this.x = 0;
-      this.y = 0;
+      this.x = width/2;
+      this.y = height/2;
     }
   }
 
+  this.smallBang = function(){
+      this.trapped=false;
+      vAux=p5.Vector.fromAngle(random(0,TWO_PI));
+      vAux.setMag(random(1,5));
+      this.v = vAux.copy();
+  }
+}
+
+
+//external method
+function mousePressed(){
+  if (allIn==true) {
+    release=true;
+    for (var i = 0; i < numPart; i++) {
+      particle[i].smallBang();
+    }
+  }
 }
